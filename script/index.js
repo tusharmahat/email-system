55e041268f8d3000d470da5150ad4ffa5e4efb1a
tusharmahat <tusharmahat@gmail.com>
2020-10-02 02:38:42 -0300

    Added New message button that shows compose on 3rd column, on clicking other buttons it compose dissappers

---
+++ script/index.js
@@ -0,0 +1,39 @@
+/**
+ * This function shows compose on the third column on click
+ * after hiding the instructions
+ * @Tushar
+ */
+function showCompose() {
+    $("#select-mail-ins").css("display", "none");
+    $("#compose").css("display", "block");
+}
+
+/**
+ * This function hides compose on the third column on click
+ * and displays the instructions
+ * @Tushar
+ */
+function showInbox() {
+    $("#compose").css("display", "none");
+    $("#select-mail-ins").css("display", "block");
+}
+
+/**
+ * This function hides compose on the third column on click
+ * and displays the instructions
+ * @Tushar
+ */
+function showDraft() {
+    $("#compose").css("display", "none");
+    $("#select-mail-ins").css("display", "block");
+}
+
+/**
+ * This function hides compose on the third column on click
+ * and displays the instructions
+ * @Tushar
+ */
+function showSent() {
+    $("#compose").css("display", "none");
+    $("#select-mail-ins").css("display", "block");
+}
